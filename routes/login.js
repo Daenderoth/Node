ea1886c3966ff669d129c7082bfe8acc27b88995
Daenderoth <62517272+Daenderoth@users.noreply.github.com>
2020-05-05 14:55:50 +0300

    Add files via upload

---
+++ routes/login.js
@@ -0,0 +1,42 @@
+const express = require('express');
+var session = require('express-session');
+const router = express.Router();
+const MD5 = require('md5');
+const con = require('../Models/mysqlCon');
+
+
+router.get('/', function(req, res, next) {
+    /* TO DO
+        Create the login form in twig
+     */
+    res.render('login', { title: 'Login' });
+});
+
+router.post('/', function(req, res, next) {
+    let {username, password} = req.body;
+    ssn = req.session;
+    /*
+        TO DO
+        1. Verify the user exists in the database;
+        2. Check the password matches (The password must be encrypted)
+        3. Save the user in the session.
+     */
+    let error = '';
+    con.checkRecordExists({
+        table:"user", args: [{col: "username", value: username},
+                             {col: "password", value: MD5(password)}]
+    },(result)=>{
+        console.log(MD5(password));
+        console.log(username);
+        console.log("user", result);
+        if(!result){
+            error= "Wrong username or password"
+        }else{
+            console.log("success");
+            ssn.user = req.body.username;
+        }
+    })
+    res.render('index', { title: 'Express' , session: req.session, user:username});
+});
+
+module.exports = router;
